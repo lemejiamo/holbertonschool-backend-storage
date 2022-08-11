61f2d962a442e81a04d15658e3c50259526e5f6b
Luis Mejia <luismejia69@gmail.com>
2022-08-11 21:04:15 +0000

    commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS `users` (
-      `id` INT PRIMARY KEY AUTO_INCREMENT,
-      `email` VARCHAR(255) NOT NULL UNIQUE,
-      `name` VARCHAR(255) NOT NULL
+CREATE TABLE If NOT EXISTS `users` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255) NOT NULL
 );
