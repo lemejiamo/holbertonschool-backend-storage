667e89910bdb797858245a1c43ead9a41392d94d
Luis Mejia <luismejia69@gmail.com>
2022-08-11 17:29:52 -0500

    comit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,8 @@
 -- because Batch 3 is the best!
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE If NOT EXISTS `users` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255) NOT NULL
+    `name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
 );
