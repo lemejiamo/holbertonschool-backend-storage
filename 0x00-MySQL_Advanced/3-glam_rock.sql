3fe5baebb6e1a1716468aae1ea19d382e9c508af
Luis Mejia <luismejia69@gmail.com>
2022-08-11 17:45:26 -0500

    fix table name in script

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,7 @@
 -- LEARNING TO USE GROUP BY
--- DONT FORGETORDER BY
-SELECT band_name, (IFNULL(split, 2022)-formed) AS lifespan FROM holberton.metal_bands WHERE style LIKE '%Glam Rock%' ORDER BY lifespan DESC;
+-- DONT FORGET TO ORDER TOO
+
+SELECT band_name, (IFNULL(split, 2022)-formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam Rock%'
+ORDER BY lifespan DESC;
