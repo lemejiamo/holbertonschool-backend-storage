e023c2b093f7ee50e2067c84eb043ab043adcab2
Luis Mejia <luismejia69@gmail.com>
2022-08-11 17:36:57 -0500

    comit

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,11 @@
 -- CREATE TABLE USERS WITH DEFAULT CONUNTRY
 -- BECAUSE WITH DEFAULT IS BEST
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE  `users` (
-       `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       `id` int NOT NULL  AUTO_INCREMENT,
        `email` varchar(255) NOT NULL UNIQUE,
        `name` varchar(255) NOT NULL,
        `country` ENUM ('US', 'CO', 'TN') DEFAULT 'US'
+	   PRIMARY KEY (`id`)
 );
