536a7f625c3cecedc7a586e5edb6e14220384ed2
Luis Mejia <luismejia69@gmail.com>
2022-08-11 17:28:35 -0500

    comit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE If NOT EXISTS `users` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
