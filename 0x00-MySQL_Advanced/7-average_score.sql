c2417b096d342415b5ae320c24f123bc00afd580
Luis Mejia <luismejia69@gmail.com>
2022-08-11 22:43:15 -0500

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,13 +2,25 @@
 -- that computes and store the average score for a student. 
 -- Note: An average score can be a decimal 
 
-DELIMITER $$
+-- DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id INT)
+-- CREATE PROCEDURE ComputeAverageScoreForUser(
+-- 	IN user_id INT)
+-- BEGIN
+-- 	SELECT SUM(score) / COUNT(score) from corrections WHERE user_id=user_id INTO @average;
+-- 	UPDATE users SET average_score=@average 
+-- 	WHERE id=user_id;
+-- END; $
+-- DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	SELECT SUM(score) / COUNT(score) from corrections WHERE user_id=user_id INTO @average;
-	UPDATE users SET average_score=@average 
-	WHERE id=user_id;
-END; $
-DELIMITER ;
+	SELECT SUM(score) / COUNT(score)
+	INTO @average_score
+	FROM corrections
+	WHERE corrections.user_id = user_id;
+	UPDATE users SET average_score = @average_score
+	WHERE id = user_id;
+END;$$
