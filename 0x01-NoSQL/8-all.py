1c844f2e14b02a50cf39678168fbc5d65ff0ae89
Luis Mejia <luismejia69@gmail.com>
2022-08-16 16:19:16 -0500

    find it all snake

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,13 +7,7 @@ def list_all(mongo_collection):
     return an empty list if no documento in the collection
     """
     documents = mongo_collection.find()
-    quantity = 0
-    documents_list = []
-    # int (type(documents))
-    for document in documents:
-        # int(document)
-        documents_list.append(document)
-        quantity = quantity + 1
-    if quantity == 0:
+    documents_list = [doc for doc in documents]
+    if documents_list.count == 0:
         return []
     return documents_list
