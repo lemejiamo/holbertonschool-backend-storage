53dec0e036b430d091964cef9f4fc462929f3518
Luis Mejia <luismejia69@gmail.com>
2022-08-17 16:42:58 -0500

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Cache class. In the __init__ method, store an instance of the Redis }
-client as a private variable named _redis (using redis.Redis()) and 
+client as a private variable named _redis (using redis.Redis()) and
 flush the instance using flushdb.
 
 """
@@ -17,15 +17,17 @@ class Cache():
     _redis.flushdb()
 
     def store(self, data):
-        import uuid
         """
         Takes a data as argumen
         Remember that data can be a str, bytes, int or float.
 
         Returns: A string
         """
+        import uuid
 
-        _randomkey = uuid.uuid4().__str__()
+        __TYPES = ['str', 'bytes', 'int', 'float']
 
-        self._redis.set(_randomkey, data)
-        return _randomkey
+        if data.__class__.__name__ in __TYPES:
+            _randomkey = uuid.uuid4().__str__()
+            self._redis.set(_randomkey, data)
+            return _randomkey
