5658df91393a29053fb64ac1805803d4fd22d961
Luis Mejia <luismejia69@gmail.com>
2022-08-17 16:51:48 -0500

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,29 +8,24 @@ flush the instance using flushdb.
 
 from redis import Redis
 from typing import Union
+import uuid
 
 
 class Cache():
     """
     Main clas
     """
-    _redis = Redis()
-    _redis.flushdb()
+    def __init__(self):
+        _redis = Redis()
+        _redis.flushdb()
 
     def store(self, data: Union['str', 'bytes', 'int', 'float']) -> str:
         """
-        Takes a data as argumen
+        Takes a data as argumen a store in a REDIS db
         Remember that data can be a str, bytes, int or float.
 
-        Returns: A string
+        Returns: A key in string format
         """
-        import uuid
-
-        __TYPES = ['str', 'bytes', 'int', 'float']
-
-        if data.__class__.__name__ in __TYPES:
-            _randomkey = uuid.uuid4().__str__()
-            self._redis.set(_randomkey, data)
-            return _randomkey
-        else:
-            return None
+        _randomkey = uuid.uuid4().__str__()
+        self._redis.set(_randomkey, data)
+        return _randomkey
