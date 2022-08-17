5ffd1d328327fb932471bd141e6f50e654fab325
Luis Mejia <luismejia69@gmail.com>
2022-08-17 16:47:01 -0500

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,7 @@ flush the instance using flushdb.
 """
 
 from redis import Redis
+from typing import Union
 
 
 class Cache():
@@ -16,7 +17,7 @@ class Cache():
     _redis = Redis()
     _redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union['str', 'bytes', 'int', 'float']) -> str:
         """
         Takes a data as argumen
         Remember that data can be a str, bytes, int or float.
@@ -31,3 +32,5 @@ class Cache():
             _randomkey = uuid.uuid4().__str__()
             self._redis.set(_randomkey, data)
             return _randomkey
+        else:
+            return None
